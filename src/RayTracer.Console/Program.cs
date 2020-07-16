e6c9f9af397fb94e6755ac378c624e32218c58d9
Łukasz Hamera <lucas.hamera@next.biz.pl>
2020-07-16 21:05:32 +0200

    Init projects

---
+++ src/RayTracer.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RayTracer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
