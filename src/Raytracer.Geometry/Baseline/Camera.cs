5d13a71f1a20a767185ac822bca3e06aae06868d
Łukasz Hamera <lucas.hamera@next.biz.pl>
2020-07-19 11:57:06 +0200

    Camera and Ligth model

---
+++ src/Raytracer.Geometry/Baseline/Camera.cs
@@ -0,0 +1,20 @@
+﻿namespace Raytracer.Geometry.Baseline
+{
+    public readonly struct Camera
+    {
+        public readonly Vec3 Position;
+        public readonly Vec3 Forward;
+        public readonly Vec3 Right;
+        public readonly Vec3 Up;
+
+        public Camera(in Vec3 position, in Vec3 lookAt)
+        {
+            Position = position;
+
+            var geometry = new BaselineGeometry();
+            Forward = geometry.Norm(lookAt - position);
+            Right = 1.5f * geometry.Norm(geometry.Cross(Forward, new Vec3(0.0f, -1.0f, 0.0f)));
+            Up = 1.5f * geometry.Norm(geometry.Cross(Forward, Right));
+        }
+    }
+}
