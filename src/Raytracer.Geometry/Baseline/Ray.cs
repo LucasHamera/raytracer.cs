b800509b857e6e4e1c089027ef54e50de9a3033f
Łukasz Hamera <43754817+LucasHamera@users.noreply.github.com>
2020-07-19 20:20:01 +0200

    Update src/Raytracer.Geometry/Baseline/Ray.cs
    Co-authored-by: Dariusz Niedoba <darksv@users.noreply.github.com>

--- src/Raytracer.Geometry/Baseline/Ray.cs
+++ src/Raytracer.Geometry/Baseline/Ray.cs
@@ -5,7 +5,7 @@
         public readonly Vec3 Start;
         public readonly Vec3 Direction;
 
-        public Ray(in Vec3 start,in Vec3 direction)
+        public Ray(in Vec3 start, in Vec3 direction)
         {
             Start = start;
             Direction = direction;
