4c2f4003f3b1a7c031247bacfcf2be684eb40719
Łukasz Hamera <lucas.hamera@next.biz.pl>
2020-07-19 12:45:13 +0200

    Interface for Surfaces

---
+++ src/Raytracer.Geometry/Common/ISurface.cs
@@ -0,0 +1,17 @@
+﻿namespace Raytracer.Geometry.Common
+{
+    public interface ISurface<out T, TVec3, out TColor>
+        where T : struct
+        where TVec3 : struct
+        where TColor : struct
+    {
+        public int Roughness
+        {
+            get;
+        }
+
+        TColor Diffuse(in TVec3 position);
+        TColor Specular(in TVec3 position);
+        T Reflect(in TVec3 position);
+    }
+}
