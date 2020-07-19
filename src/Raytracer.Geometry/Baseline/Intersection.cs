c5c075c487f6db776b39277a4e93539a35e5feb9
Łukasz Hamera <lucas.hamera@next.biz.pl>
2020-07-19 22:04:07 +0200

    Init Intersection

---
+++ src/Raytracer.Geometry/Baseline/Intersection.cs
@@ -0,0 +1,19 @@
+﻿using Raytracer.Geometry.Common;
+
+namespace Raytracer.Geometry.Baseline
+{
+    public readonly struct Intersection<THitable>
+        where THitable: struct, IHitable
+    {
+        public readonly THitable Thing;
+        public readonly Ray Ray;
+        public readonly float Distance;
+
+        public Intersection(in THitable thing, in Ray ray, in float distance)
+        {
+            Thing = thing;
+            Ray = ray;
+            Distance = distance;
+        }
+    }
+}
