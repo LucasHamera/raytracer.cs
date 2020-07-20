145aa9cc8c07e25127ba088e1031c17b5bca88c1
Łukasz Hamera <lucas.hamera@next.biz.pl>
2020-07-20 21:31:28 +0200

    Updated IHitable

---
+++ src/Raytracer.Geometry/Baseline/Hitable/IHitable.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using Raytracer.Geometry.Common;
+
+namespace Raytracer.Geometry.Baseline.Hitable
+{
+    public interface IHitable<TSurface, THitable>
+        where TSurface: struct, ISurface<float, Vec3, Color>
+        where THitable : struct, IHitable<TSurface, THitable>
+    {
+        public TSurface Surface
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get; 
+        }
+
+        Optional<Intersection<THitable, TSurface>> Intersect(in Ray ray);
+
+        Vec3 Normal(in Vec3 position);
+    }
+}
