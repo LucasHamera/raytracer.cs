922b4f3ca04b74a4d98ad2e9c9a303c5ac9255ee
darksv <darek969-12@o2.pl>
2020-07-20 23:29:37 +0200

    Canvas fixes

--- src/Raytracer/Canvas/Canvas.cs
+++ src/Raytracer/Canvas/Canvas.cs
@@ -6,23 +6,33 @@ namespace Raytracer.Canvas
     public class Canvas
     {
         private readonly Color[] _pixels;
-        
+
         public int Width { get; }
         public int Height { get; }
-        
-        public Span<Color> Data => _pixels.AsSpan();
-        
+
+        public ReadOnlySpan<Color> Data => _pixels.AsSpan();
+
         public Color this[int x, int y]
         {
-            get => _pixels[y * Width + x];
-            set => _pixels[y * Width + x] = value;
+            get
+            {
+                if (x < 0 || x >= Width) throw new ArgumentException(nameof(x));
+                if (y < 0 || y >= Height) throw new ArgumentException(nameof(y));
+                return _pixels[y * Width + x];
+            }
+            set
+            {
+                if (x < 0 || x >= Width) throw new ArgumentException(nameof(x));
+                if (y < 0 || y >= Height) throw new ArgumentException(nameof(y));
+                _pixels[y * Width + x] = value;
+            }
         }
-        
+
         public Canvas(int width, int height)
         {
             if (width <= 0) throw new ArgumentException(nameof(width));
             if (height <= 0) throw new ArgumentException(nameof(height));
-            
+
             _pixels = new Color[width * height];
             Width = width;
             Height = height;
