69ac71de88a105031a96cab7c5709c1df3262745
Łukasz Hamera <lucas.hamera@gmail.com>
2020-07-21 22:33:42 +0200

    Benchmark project

---
+++ benchmarks/Raytracer.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Raytracer.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BaselineRayTracerBenchmark>();
+        }
+    }
+}
