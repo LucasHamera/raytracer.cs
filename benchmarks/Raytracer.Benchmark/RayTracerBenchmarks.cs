88668b30fd8e040beb2b27dcdfeb847f5f70e8c3
Łukasz Hamera <lucas.hamera@gmail.com>
2020-08-08 16:15:51 +0200

    Fixed namespace, and removed empty space in RayTracerBenchmarks

--- benchmarks/Raytracer.Benchmark/RayTracerBenchmarks.cs
+++ benchmarks/Raytracer.Benchmark/RayTracerBenchmarks.cs
@@ -1,13 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Raytracer.Geometry.Scenes;
-using RayTracer = RayTracer.RayTracer;
 
 namespace Raytracer.Benchmark
 {
     public class RayTracerBenchmarks
     {
         private BaseRayTracer _baseRayTracer;
-        private global::RayTracer.RayTracer _rayTracer;
+        private RayTracer.RayTracer _rayTracer;
         private MyScene _myScene;
 
         [Params(512)]
@@ -17,14 +16,12 @@ namespace Raytracer.Benchmark
             set;
         }
 
-
         [GlobalSetup]
         public void Setup()
         {
             _baseRayTracer = new BaseRayTracer(RenderSize, RenderSize);
-            _rayTracer = new global::RayTracer.RayTracer(RenderSize, RenderSize);
+            _rayTracer = new RayTracer.RayTracer(RenderSize, RenderSize);
             _myScene = new MyScene();
-
         }
 
         [Benchmark]
@@ -32,7 +29,6 @@ namespace Raytracer.Benchmark
         {
             var canvas = _baseRayTracer.Render(_myScene);
         }
-
 
         [Benchmark]
         public void Improved()
